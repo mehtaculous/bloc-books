932ddbd44c1a0fa5325b74a8d5360ed77ceddf3e
Sahil Mehta <samehta91@gmail.com>
2017-07-18 21:52:11 -0700

    Add a show model, view and controller to Book

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
-gem 'bloc_works', path: "../bloc-works"
-gem "rack",       "~> 1.6"
+gem 'bloc_works', path: '../bloc-works'
+gem 'bloc_record', path: '../bloc-record'
+gem 'rack', "~> 1.6"
+gem 'rake'
+gem 'sqlite3'
+gem 'pry'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class BooksController < BlocWorks::Controller
   def welcome
     @book = "Eloquent Ruby"
@@ -5,21 +7,35 @@ class BooksController < BlocWorks::Controller
   end
 
   def index
+    @books = Book.all
+    render :index, books: @books
   end
 
   def show
+    binding.pry
+    @book = Book.find(params[:id])
+    render :show, book: @book
   end
 
   def new
-  end
-
-  def create
-  end
-
-  def update
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    request = Rack::Request.new(@env)
+
+    @book = Book.create(
+      name: request.params[:name],
+      author: request.params[:author],
+      pages: request.params[:pages]
+    )
+  end
+
+  def update
   end
 
   def delete
