621ec8fef43ec0e95c517dc82f2c09285af81590
Sahil Mehta <samehta91@gmail.com>
2017-07-27 15:02:11 -0700

    Update render to automatically map view to controller actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,26 +8,48 @@ class BooksController < BlocWorks::Controller
 
   def index
     @books = Book.all
-    render :index
+    render
   end
 
   def show
     @book = Book.find(params['id'].to_i)
-    render :show
+    render
   end
 
   def new
+    @book = Book.new
+    render
   end
 
   def edit
+    @book = Book.find(params['id'].to_i)
+    render
   end
 
   def create
+    @book = Book.new
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   def update
+    @book = Book.find(params['id'].to_i)
+    if @book.update_attributes
+      redirect_to @book
+    else
+      render :edit
+    end
   end
 
   def delete
+    @book = Book.find(params['id'].to_i)
+    if @book.destroy
+      redirect_to '/books'
+    else
+      render :show
+    end
   end
 end
--- config.ru
+++ config.ru
@@ -5,13 +5,9 @@ app = BlocWorks::Application.new
 use Rack::ContentType
  
 app.route do
-  map "", "books#welcome"
-  map ":controller/:id/:action"
-  map ":controller/:id", default: { "action" => "show" }
-  map ":controller", default: { "action" => "index" }
-
-  # resources :books
-  # resources :libraries
+  map "", "books#welcome", default: {:request_method => "GET"}
+  resources :books
+  resources :libraries
 end
  
 run(app)
